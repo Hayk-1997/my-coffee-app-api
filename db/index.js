ebd6b6e430234a2e1753a72ac4f10243033f87e2
Hayk <abrahamyanhayk01@gmail.com>
2020-08-23 21:27:03 +0400

    Get coffee services data with graphQl

--- db/index.js
+++ db/index.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 // Connect to database
 const connect = () => {
-    const options = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    };
-    mongoose.set('useCreateIndex', true);
-    mongoose.connect(process.env.MONGO_URI, options);
-    return mongoose.connection
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  };
+  mongoose.set('useCreateIndex', true);
+  mongoose.connect(process.env.MONGO_URI, options);
+  return mongoose.connection;
 };
 module.exports = connect;
---
+++ graphQl/Services/type.js
@@ -0,0 +1,15 @@
+module.exports = `
+    type Services {
+        _id: String
+        en: Services!
+        am: Services!
+        box1: Services!
+        box2: Services!
+        box3: Services!
+        title: String!
+        description: String!
+        icon: Services!
+        item: Services!
+        preview_url: String!
+    }
+`;
